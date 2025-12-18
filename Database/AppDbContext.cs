83957a68546e8a9a870e172ec3013f1df2d9f36c
须臾百梦 <jiahong2874@gmail.com>
2025-12-19 00:13:05 +0800

    Database

---
+++ Database/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Hotel_Room_Booking_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Room_Booking_API.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Room> Rooms => Set<Room>();
+        public DbSet<Booking> Bookings => Set<Booking>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Room>().HasData(
+                new Room { Id = 1, Name = "101", Type = "Single", IsAvailable = true },
+                new Room { Id = 2, Name = "102", Type = "Single", IsAvailable = true },
+                new Room { Id = 3, Name = "201", Type = "Double", IsAvailable = true },
+                new Room { Id = 4, Name = "301", Type = "Suite", IsAvailable = true }
+            );
+        }
+    }
+}
