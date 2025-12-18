09b862279a12a6e870b7484b8daaeb7b112b152c
须臾百梦 <jiahong2874@gmail.com>
2025-12-18 22:16:15 +0800

    Create Model

---
+++ Model/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace Hotel_Room_Booking_API.Model
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; } = "";
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
---
+++ Model/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Room_Booking_API.Model
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Type { get; set; } = "";
+        public bool IsAvailable { get; set; } = true;
+    }
+}
